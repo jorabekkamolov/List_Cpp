7f7790b977b24327c8e948acb5cb648a69480c67
jorabek <turnerko>
2024-11-02 14:45:05 +0500

    insert iterator begin end

--- headers/list_cpp.h
+++ headers/list_cpp.h
@@ -24,14 +24,51 @@ private:
 
 public:
   List();
+  List(std::initializer_list<value_type>);
   ~List();
   void push_back(value_type);
   void pop_back();
+  void push_front(value_type);
+
   size_type size();
+  class iterator;
+  iterator begin();
+  iterator end();
+  iterator insert(iterator, value_type);
+
+  class iterator {
+  public:
+    using value_type = T;
+    using pointer = T *;
+
+  private:
+    Node *current;
+
+  public:
+    friend class List;
+    iterator(Node *current) : current(current) {}
+    value_type operator*() { return current->value; }
+    iterator operator++() {
+      if (current) {
+        current = current->next;
+      }
+      return *this;
+    }
+    bool operator==(iterator other) { return current == other->current; }
+    bool operator!=(iterator other) { return current != other->current; }
+  };
 };
 
 template <typename T>
 List<T>::List() : head(nullptr), tail(nullptr), size_list(0) {}
+
+template <typename T>
+List<T>::List(std::initializer_list<value_type> list)
+    : head(nullptr), tail(nullptr), size_list(0) {
+  for (auto it = list.begin(); it < list.end(); it++) {
+    push_back(*it);
+  }
+}
 
 template <typename T> List<T>::~List() {
   while (head) {
@@ -70,6 +107,51 @@ template <typename T> void List<T>::push_back(value_type value) {
 
 template <typename T> typename List<T>::size_type List<T>::size() {
   return size_list;
+}
+
+template <typename T> void List<T>::push_front(value_type value) {
+  Node *new_node = new Node(value);
+  if (!head) {
+    head = new_node;
+    tail = new_node;
+  } else {
+    new_node->next = head;
+    head = new_node;
+  }
+  size_list++;
+}
+
+template <typename T> typename List<T>::iterator List<T>::begin() {
+  return iterator(head);
+}
+
+template <typename T> typename List<T>::iterator List<T>::end() {
+  return iterator(nullptr);
+}
+
+template <typename T>
+typename List<T>::iterator List<T>::insert(iterator pos, value_type value) {
+  Node *node_pos = pos.current;
+  Node *new_node = new Node(value);
+  if (node_pos == head) {
+    if (!head) {
+      head = new_node;
+      tail = new_node;
+      new_node->next = nullptr;
+    } else {
+      new_node->next = head;
+      head = new_node;
+    }
+  } else {
+    Node *current = head;
+    while (current->next != node_pos) {
+      current = current->next;
+    }
+    new_node->next = current->next;
+    current->next = new_node;
+  }
+  size_list++;
+  return iterator(new_node);
 }
 
 #endif
--- tests/test.cpp
+++ tests/test.cpp
@@ -7,6 +7,45 @@ TEST(List_test, init) {
   EXPECT_EQ(l.size(), 2);
 }
 
+TEST(List_test, iterators) {
+  List<int> l = {1, 2, 3, 4, 5};
+  List<int>::iterator it = l.begin();
+  ++it;
+  EXPECT_EQ(*it, 2);
+}
+
+TEST(List_test, insert1) {
+  List<int> l = {1, 2, 3, 4};
+  auto it = l.begin();
+  it = l.insert(it, 12);
+
+  EXPECT_EQ(*it, 12);
+}
+
+TEST(List_test, insert2) {
+  List<int> l = {1};
+  auto it = l.begin();
+  it = l.insert(it, 12);
+
+  EXPECT_EQ(*it, 12);
+}
+
+TEST(List_test, insert3) {
+  List<int> l = {1, 2, 3, 4};
+  auto it = l.begin();
+  ++it;
+  ++it;
+  it = l.insert(it, 12);
+  EXPECT_EQ(*it, 12);
+}
+
+TEST(List_test, insert4) {
+  List<int> l;
+  auto it = l.begin();
+  it = l.insert(it, 12);
+  EXPECT_EQ(*it, 12);
+}
+
 int main(int argc, char *argv[]) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
